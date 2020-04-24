76aab33cc29f65c77bb75b0c3342942aba9f1084
WenhaoCui <wenhaocu@usc.edu>
2020-04-23 19:58:33 -0700

    glpk
    glpk

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,39 @@
 "use strict";
 
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 const solver = require("javascript-lp-solver");
-
+const bodyparser = require("body-parser")
 // We need cors middleware to bypass CORS security in browsers.
 const cors = require("cors");
+const morgan = require("morgan");
+require('shelljs/global');
+const fs = require('fs')
+var FileSaver = require('file-saver'); 
+
+
+
+
+// Sync call to exec()
+var version = exec('node --version', {silent:true}).output;
+
+
+//enable file uploaded
+app.use(
+  fileUpload({
+    createParentPath: true
+  })
+);
 
 app.use(express.static("static"));
 app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(morgan("dev"));
 
-let port = 5000;
 
+const port = 5000;
 
 /**
  * Solve the model by jsLPSolver
@@ -27,10 +49,9 @@ function LinearProgramming(model, solver) {
 const delay = function(t) {
   return new Promise(resolve => setTimeout(resolve, t));
 };
-
-
 /**
- * The default path
+ * The default path is for http get method, 
+ * sending message by url
  */
 app.get("/", async function(req, res) {
   if (req.query && Object.keys(req.query).length > 0) {
@@ -38,6 +59,155 @@ app.get("/", async function(req, res) {
     handleGet(res, res, req.query);
   }
 });
+
+app.post("/upload", async function(req, res){
+
+  let error = "NO_ERROR";
+  let output_lp;
+  let output;
+  console.log("req", req);
+
+// 上传文档时的写法，已放弃
+  //console.log("req.samplefile:", req.files.sampleFile);
+  //console.log(JSON.parse(req.files.sampleFile.data));
+//   try{
+//     if(!req.files){
+//       res.send({
+//         status: false,
+//         message: "No file is uploaded"
+//       });
+//     }else{
+//       if(req.files !== undefined){
+        
+//         let model_post = req.files.sampleFile;
+//         model_post.mv("./uploads/" + model_post.name);
+//         let input = JSON.parse(model_post.data);
+//         //const input_json = require(`../backend/uploads/${model_post.name}`);
+//         output_lp = LinearProgramming(input, solver);
+//         console.log(output_lp);
+//       }else{error = "ERROR: uploaded file isn't in JSON type";}
+
+//       output = {
+//         status: true,
+//         message: "File is uploaded",
+//         output_lp: output_lp,
+//         error: error
+//       };
+      
+//       let outputString = JSON.stringify(output, null, 2);
+//       console.log("outputString: ", outputString);
+
+//       await delay(1000);
+      
+//       res.send(output);
+//     }
+//   }catch(err){
+//     res.status(404).send(err);
+//     console.log("error in app post:", err);
+//   }
+// })
+try{  
+    if(req.body !== undefined){
+      
+      let model_post = req.body;
+      console.log("model_post", typeof model_post);
+
+      console.log("model_post is ", model_post);
+      let input = JSON.parse(Object.keys(model_post));
+      console.log("input", input);
+      //const input_json = require(`../backend/uploads/${model_post.name}`);
+      output_lp = LinearProgramming(input, solver);
+      console.log(output_lp);
+    }else{error = "ERROR: uploaded INFO isn't in JSON type";}
+
+    output = {
+      status: true,
+      message: "JSON is uploaded",
+      output_lp: output_lp,
+      error: error
+    };
+    
+    let outputString = JSON.stringify(output, null, 2);
+    console.log("outputString: ", outputString);
+
+    await delay(1000);
+    
+    res.send(output);
+
+}catch(err){
+  res.status(404).send(err);
+  console.log("error in app post:", err);
+}
+})
+
+
+app.post("/glpk", async function(req, res){
+
+  let error = "NO_ERROR";
+  let output_lp;
+  let output;
+
+  let model_post = req.body;
+  console.log("model_post", typeof model_post);
+
+  console.log("model_post is ", model_post);
+
+  let input =  Object.values(model_post)+"";
+  let replac_str = "%2B";
+  input = input.split(replac_str).join("+");
+  console.log("input:", input);
+  
+
+  fs.writeFile("test.txt", input, (err, data) => {
+    if (err) throw err;
+  })
+
+  await delay(100);
+
+  // Async call to exec()
+  exec('glpsol -m test.txt -o temp.txt');
+
+
+
+  try{
+    if(!req.body){
+      res.send({
+        status: false,
+        message: "No file is uploaded"
+      });
+    }else{
+      if(req.body !== undefined){
+        
+  //       let model_post = req.files.solution;
+  //       model_post.mv("./uploads/" + model_post.name);
+        try {
+          const data = fs.readFileSync('temp.txt', 'utf8');
+          output_lp = data;
+        } catch (err) {
+          console.error(err);
+        }
+      }else{error = "ERROR: uploaded file isn't in JSON type";}
+
+      output = {
+        status: true,
+        message: "File is uploaded",
+        output_lp: output_lp,
+        error: error
+      };
+      
+      let outputString = JSON.stringify(output, null, 2);
+      console.log("outputString: ", outputString);
+
+      await delay(1000);
+      
+      res.send(output);
+    }
+  }catch(err){
+    res.status(404).send(err);
+    console.log("error in app post:", err);
+  }
+})
+
 
 app.listen(port, err => {
   console.log(`Listening on port: ${port}`);
@@ -52,7 +222,6 @@ app.listen(port, err => {
 async function handleGet(req, res, query) {
   let error = "NO_ERROR";
   let output_lp;
-  let model;
 
   console.log("query: ", query);
   //console.log(query.model);
@@ -61,12 +230,13 @@ async function handleGet(req, res, query) {
     query !== undefined &&
     query.model !== undefined
   ) {
+    //console.log(model_get);
     // Convert min_value and max_value from String to integer
     let model_get = JSON.parse(query.model);
     
     // Generate a random number
     output_lp = LinearProgramming(model_get, solver);
-    console.log("output is: ", output_lp);
+    console.log("Via http get method, output is: ", output_lp);
   } else {
     error = "ERROR: input not provided";
   }
@@ -76,16 +246,24 @@ async function handleGet(req, res, query) {
     output_lp: output_lp,
     error: error
   };
-
-  // Convert output to JSON
+  // Convert output to string
   let outputString = JSON.stringify(output, null, 2);
   console.log("outputString: ", outputString);
-  
-  // Run an external 
-  // output
+  // console.log(typeof outputString);
   // Let's generate some artificial delay!
-  await delay(2000);
-
+  await delay(1000);
   // Send it back to the frontend.
   res.send(outputString);
 }
+
+
+// require('shelljs/global');
+
+// // Sync call to exec()
+// var version = exec('node --version', {silent:true}).output;
+
+// // Async call to exec()
+// exec('glpsol --cuts --fpump --mipgap 0.001 --model glpk_example/problem.mod -o glpk_example/temp.txt', function(status, output) {
+//   console.log('Exit status:', status);
+//   console.log('Program output:', output);
+// });
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,9 +1,41 @@
 /**
- * Set the initial values of min_value and max_value
+ * On click the tab, show one content and hide another one
+ * 点击分隔页时，仅显示LP-solver或者GLPK中的一个
+ */
+function openLP(evt, choose_solver) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(choose_solver).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/**
+ * Set the initial values of linear programming functions
+ * Both GLPK and LP SOlver
+ * Math Prog for GLPK is multi-lined
  */
 function initialize() {
-  document.getElementById("min_value").value = "0";
-  document.getElementById("max_value").value = "100";
+  document.getElementById("objective_func").value = "3x + y";
+  document.getElementById("subject_to_1").value = "2x - y <= 4";
+  document.getElementById("subject_to_2").value = "2x + 3y <= 12";
+  document.getElementById("subject_to_3").value = "y <= 3";
+  document.getElementById("varibales").value = "x y";
+  document.getElementById("input_glpk").value = 
+  "var x1;\nvar x2;\nmaximize obj: 0.6 * x1 + 0.5 * x2;\ns.t. c1: x1 + 2 * x2 <= 1;\ns.t. c2: 3 * x1 + x2 <= 2;\nsolve;\ndisplay x1, x2;\nend;";
 
   let loader = document.getElementById("loader");
   loader.style.display = "none";
@@ -13,47 +45,90 @@ function initialize() {
 initialize();
 
 /**
+ * 通过正则表达式
+ * 检查输入格式，返回JSON或者false，用在LP-solver中
+ * This Function is used in submit_lp
+ * Check whether the input is legal or not
+ * if all inputs are legal, translate parameters into a JSON,
+ * or return false
+ */
+function input_to_json(variables, obj, sub1, sub2, sub3, sub4, sub5, sub6, max_or_min, integer){
+
+  return input_json;
+}
+
+
+/**
  * Handle the click event on Submit (Generate) button
  */
-document.getElementById("submit").onclick = function() {
-  submit();
+document.getElementById("submit_lp").onclick = function() {
+  submit_lpsolver();
 };
+
+document.getElementById("submit_glpk").onclick = function(){
+  submit_glpk();
+}
 
 /**
  * An async function to send the request to the backend.
+ * Dealt data By LP-solver, which is a popular npm package
+ * As a package, it requires a JSON as the final input and return an output JSON
+ * https://www.npmjs.com/package/javascript-lp-solver
  */
-async function submit() {
+async function submit_lpsolver() {
   console.log("In submit!");
 
   // Set the mouse cursor to hourglass
   document.body.style.cursor = "wait";
 
   // Accessing the div that has random value 
-  let random_value_element = document.getElementById("random-value");
+  let solution_lp = document.getElementById("result_by_lp_solver");
 
-  random_value_element.innerHTML = "Please wait...";
+  solution_lp.innerHTML = "Please wait...";
   
   // Show the loader element (spinning wheels)
   let loader = document.getElementById("loader");
   loader.style.display = "inline-block";
 
   try {
-    // Get the min/max values from the user 
-    let min_value = document.getElementById("min_value").value;
-    let max_value = document.getElementById("max_value").value;
+    let variables = document.getElementById("varibales").value;
+    let obj = document.getElementById("objective_func").value;
+    let sub1 = document.getElementById("subject_to_1").value;
+    let sub2 = document.getElementById("subject_to_2").value;
+    let sub3 = document.getElementById("subject_to_3").value;
+    let sub4 = document.getElementById("subject_to_4").value;
+    let sub5 = document.getElementById("subject_to_5").value;
+    let sub6 = document.getElementById("subject_to_6").value;
+    let max_or_min = document.getElementById("optimize").value;
+    let integer = document.getElementById("integer_solution").value;
 
 
-    let request = `http://127.0.0.1:5000/?min_value=${min_value}&max_value=${max_value}`;
-    console.log("request: ", request);
+    // an example of input_json
+    let input_json = {
+      optimize: 'maximize',
+      opType: 'max',
+      constraints: { c1: { max: 4 }, c2: { max: 12 }, c3: { max: 3 } },
+      variables: {
+        x: { maximize: 3, c1: 2, c2: 2, c3: 0 },
+        y: { maximize: 1, c1: -1, c2: 3, c3: 1 }
+      }
+    };
+    input_json = JSON.stringify(input_json);
+    //let request_get = `http://127.0.0.1:5000/?model=${input_json}`;
+    //console.log("request via HTTP GET method: ", request_get);
+    let request_post = `http://127.0.0.1:5000/upload`;
+    console.log("request via HTTP POST method: ", request_post);
+
 
     // Send an HTTP GET request to the backend
-    const data = await axios.get(request);
+    //const data = await axios.get(request_get);
+    //console.log(data);
 
-    console.log("data.data: ", JSON.stringify(data.data, null, 2));
+    // Send an HTTP Post request to the backend
+    const data1 = await axios.post(request_post, input_json);
+    console.log(data1);
+    solution_lp.innerHTML = "Here is your solution: " + JSON.stringify(data1.data.output_lp);
     
-
-    // Display the random value
-    random_value_element.innerHTML = "Here is your random number: " + data.data.randomValue;
   } catch (error) {
     console.log("error: ", error);
   }
@@ -64,3 +139,52 @@ async function submit() {
   // Hide loader animation
   loader.style.display = "none";
 }
+
+async function submit_glpk() {
+  console.log("In submit!");
+
+  // Set the mouse cursor to hourglass
+  document.body.style.cursor = "wait";
+
+  // Accessing the div that has random value 
+  let solution_glpk = document.getElementById("solution_glpk");
+
+  solution_glpk.innerHTML = "Please wait...";
+  
+  // Show the loader element (spinning wheels)
+  let loader = document.getElementById("loader");
+  loader.style.display = "inline-block";
+
+  try {
+    let content = {
+      input_1 : document.getElementById("input_glpk").value};
+
+    console.log("input_glpk:"+content);
+
+
+    //let request_get = `http://127.0.0.1:5000/?model=${input_json}`;
+    //console.log("request via HTTP GET method: ", request_get);
+    let request_post = `http://127.0.0.1:5000/glpk`;
+    console.log("request via HTTP POST method: ", request_post);
+
+
+    // Send an HTTP GET request to the backend
+    //const data = await axios.get(request_get);
+    //console.log(data);
+
+    // Send an HTTP Post request to the backend
+    const data1 = await axios.post(request_post, content);
+    console.log(data1);
+    solution_glpk.innerHTML = data1.data.output_lp;
+    
+  } catch (error) {
+    console.log("error: ", error);
+  }
+
+  // Set the cursor back to default
+  document.body.style.cursor = "default";
+
+  // Hide loader animation
+  loader.style.display = "none";
+}
+
